0615200ca52572dc6892ab5b048e61fe93d15d1d
Alex Bortnik <bortnik.ar@gmail.com>
2022-11-08 01:40:18 +0300

    feat: copy files from directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const path = require('path');
+const { mkdir, readdir, copyFile, rm, access } = require('fs').promises;
 
+async function deleteDir(path) {
+  try {
+    await access(path);
+    await rm(path, { recursive: true });
+  } catch(err) {
+    console.log('No folder found.');
+  }
+}
+
+async function copyDir(source, target) {
+  await deleteDir(target);
+  await mkdir(target, { recursive: true });
+
+  const dirEntries = await readdir(source, { withFileTypes: true });
+  const filesToCopy = dirEntries.filter(entry => entry.isFile());
+
+  filesToCopy.forEach(({ name: fileName }) => copyFile(
+    path.join(source, fileName),
+    path.join(target, fileName)
+  ));
+
+  console.log('Copied successfully!');
+}
+
+copyDir(
+  path.join(__dirname, 'files'),
+  path.join(__dirname, 'files-copy')
+);
