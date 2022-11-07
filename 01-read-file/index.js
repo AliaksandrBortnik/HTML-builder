c49f5b7a9d81baf6ef264602cb2c3285b0a3ecd6
Alex Bortnik <bortnik.ar@gmail.com>
2022-11-07 17:14:23 +0300

    refactor: minor improve for file reading

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 
-const rs = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const filePath = path.join(__dirname, 'text.txt');
+const rs = fs.createReadStream(filePath, 'utf-8');
 rs.on('data', text => process.stdout.write(text));
