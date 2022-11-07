b10c91fa6fb32d57bc6f145171bcfee6d959e498
Alex Bortnik <bortnik.ar@gmail.com>
2022-11-07 18:22:40 +0300

    feat: read console and write to file

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileName = 'text.txt';
+const filePath = path.join(__dirname, fileName);
+const output = fs.createWriteStream(filePath, 'utf-8');
+
+const endCommand = 'exit';
+const endPhrase = 'Finish. Everything is done.';
+
+const exit = () => {
+  console.log(endPhrase);
+  process.exit();
+};
+
+console.log('Please, enter any text you want.');
+
+process.stdin.on('data', (text) => {
+  if (text.toString().trim() === endCommand) {
+    exit();
+  }
+
+  output.write(text);
+});
+
+// Exit on CTRL+C
+process.on('SIGINT', exit);
