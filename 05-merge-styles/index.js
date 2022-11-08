ebff4900f4927454bd678352c5b5c52f9d28ccfc
Alex Bortnik <bortnik.ar@gmail.com>
2022-11-08 12:57:28 +0300

    feat: minor improve. split merged styles by new line

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 async function makeStyleBundle(targetFolderName, targetFileName) {
   const targetStream = fs.createWriteStream(
-    path.join(__dirname, targetFolderName, targetFileName)
+    path.join(__dirname, targetFolderName, targetFileName),
+    'utf-8'
   );
 
   function isCssFileExt(fileName) {
@@ -14,10 +15,10 @@ async function makeStyleBundle(targetFolderName, targetFileName) {
   const dirEntries = await fs.promises.readdir(sourceFolderPath, { withFileTypes: true });
   const cssFiles = dirEntries.filter(entry => entry.isFile() && isCssFileExt(entry.name));
 
-  cssFiles.forEach(file =>
-    fs.createReadStream(path.join(sourceFolderPath, file.name))
-      .pipe(targetStream)
-  );
+  for (const file of cssFiles) {
+    const content = await fs.promises.readFile(path.join(sourceFolderPath, file.name), 'utf-8');
+    targetStream.write(`${content}\n`);
+  }
 }
 
 makeStyleBundle('project-dist', 'bundle.css');
