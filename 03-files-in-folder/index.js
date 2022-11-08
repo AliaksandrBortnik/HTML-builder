37ef5b63adfab540073dab07b60c843a55ca41bd
Alex Bortnik <bortnik.ar@gmail.com>
2022-11-08 11:50:43 +0300

    refactor: error handling for directory file details

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,23 @@
 const fs = require('fs').promises;
-const path = require('path');
+const { join, extname } = require('path');
 
 const folderName = 'secret-folder';
-const folderPath = path.join(__dirname, folderName);
+const folderPath = join(__dirname, folderName);
 
 fs.readdir(folderPath, { withFileTypes: true })
   .then(dirEntries => {
     dirEntries.forEach(entry => {
       if (entry.isFile()) {
-        const filePath = path.join(folderPath, entry.name);
+        const filePath = join(folderPath, entry.name);
 
         fs.stat(filePath)
           .then((stats) => {
             const fileName = entry.name.slice(0, entry.name.lastIndexOf('.'));
-            const fileExtension = path.extname(entry.name).slice(1);
+            const fileExtension = extname(entry.name).slice(1);
             const fileSizeKB = (stats.size / 1024).toFixed(2);
             console.log(`${fileName}-${fileExtension}-${fileSizeKB}KB`);
           });
       }
     });
-  });
+  })
+  .catch(err => console.log(err.message));
